eb23b89201469085bd53f67dc3647432758309ad
prasath <prasath304@gmail.com>
2026-03-07 02:46:34 +0530

    angular update 20

---
+++ tinyweb-app/src/environment/environment.staging.ts
@@ -0,0 +1,7 @@
+// src/environments/environment.staging.ts
+export const environment = {
+    production: false,
+    apiUrl: 'https://staging-api.myapp.com',
+    featureFlag: true
+  };
+  
--- tinyweb-app/src/main.server.ts
+++ tinyweb-app/src/main.server.ts
@@ -1,8 +1,8 @@
+import { provideServerRendering } from '@angular/ssr';
 import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { provideServerRendering } from '@angular/platform-server';
 import { provideHttpClient } from '@angular/common/http';
 
 export default function bootstrap(context: BootstrapContext) {
