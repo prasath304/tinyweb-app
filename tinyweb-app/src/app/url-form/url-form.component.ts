7657a83b122048f8515e2b6ed4175e1bddfa3140
prasath <prasath304@gmail.com>
2026-03-07 15:53:56 +0530

    url form & url list added

--- tinyweb-app/src/app/url-form/url-form.component.ts
+++ tinyweb-app/src/app/url-form/url-form.component.ts
@@ -1,11 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-url-form',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './url-form.component.html',
   styleUrl: './url-form.component.scss',
 })
 export class UrlFormComponent {
+ url = '';
+  isPrivate = false;
 
+  constructor() {}
+
+  submitUrl() {
+    // this.http.post('/api/shorten', { url: this.url, private: this.isPrivate })
+    //   .subscribe(res => {
+    //     console.log('Short link created:', res);
+    //     alert('Short link generated successfully!');
+    //   });
+  }
 }
--- tinyweb-app/src/app/url-list/url-list.component.ts
+++ tinyweb-app/src/app/url-list/url-list.component.ts
@@ -1,11 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-url-list',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './url-list.component.html',
   styleUrl: './url-list.component.scss',
 })
 export class UrlListComponent {
+ //links: any[] = [];
+ links = [
+  { id: 1, shortUrl: 'http://sho.rt/abc', clicks: 12 },
+  { id: 2, shortUrl: 'http://sho.rt/xyz', clicks: 5 }
+];
 
+  searchTerm = '';
+urlLink: any;
+
+  constructor() {
+    this.loadLinks();
+  }
+
+  loadLinks() {
+   // this.http.get<any[]>('/api/links').subscribe(data => this.links = data);
+  }
+
+  filteredLinks() {
+   // return this.links.filter(l => l.shortUrl.includes(this.searchTerm));
+   return this.links;
+  }
+
+  copyToClipboard(url: string) {
+    navigator.clipboard.writeText(url);
+    alert('Copied: ' + url);
+  }
+
+  deleteUrl(id: number) {
+    // this.http.delete(`/api/links/${id}`).subscribe(() => {
+    //   this.links = this.links.filter(l => l.id !== id);
+    // });
+  }
 }
