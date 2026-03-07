1be37c7ab2fe4b431e016514b3578c998e9fdc51
prasath <prasath304@gmail.com>
2026-03-07 20:35:02 +0530

    main server config changes

--- tinyweb-app/src/main.server.ts
+++ tinyweb-app/src/main.server.ts
@@ -2,5 +2,17 @@ import { bootstrapApplication, BootstrapContext } from '@angular/platform-browse
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideServerRendering } from '@angular/platform-server';
+import { routes } from './app/app.routes';
 
-export default bootstrapApplication(AppComponent, appConfig);
+export default function bootstrap(context: BootstrapContext) {
+  return bootstrapApplication(AppComponent, {
+    providers: [
+      provideRouter(routes),
+      provideHttpClient(),
+      provideServerRendering()
+    ]
+  }, context); // <-- context passed here
+}
